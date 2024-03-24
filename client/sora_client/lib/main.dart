0492ca69cec440795800a761f89d8d8973f1a8e4
SoraKang <70902065+sora9z@users.noreply.github.com>
2024-03-24 22:38:19 +0900

    [client] add search result ui

--- client/sora_client/lib/main.dart
+++ client/sora_client/lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Future<List<Map<String, dynamic>>>? searchResult;
+
+  Future<List<Map<String, dynamic>>> searchAddress(String query) async {
+    final url = Uri.parse('http://localhost:3000/address/search?query=$query');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List<dynamic> jsonResponse = jsonDecode(response.body);
+      return jsonResponse.cast<Map<String, dynamic>>();
+    } else {
+      throw Exception('Failed to load address');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TextEditingController 인스턴스 생성
     final TextEditingController controller = TextEditingController();
 
     return MaterialApp(
@@ -24,7 +42,7 @@ class MainApp extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextField(
-                  controller: controller, // TextField에 controller 설정
+                  controller: controller,
                   decoration: const InputDecoration(
                     hintText: '주소를 입력하세요',
                     border: OutlineInputBorder(),
@@ -33,24 +51,37 @@ class MainApp extends StatelessWidget {
               ),
               ElevatedButton(
                 onPressed: () {
-                  // controller를 사용하여 TextField의 텍스트에 접근
-                  final query = controller.text;
-                  final url = Uri.parse(
-                      'http://localhost:3000/address/search?query=$query');
-                  http.get(url).then((response) {
-                    if (response.statusCode == 200) {
-                      // 성공적으로 데이터를 받아왔을 때의 처리
-                      print('검색 결과: ${response.body}');
-                    } else {
-                      // 서버 오류 처리
-                      print('서버 오류: ${response.statusCode}');
-                    }
-                  }).catchError((error) {
-                    // 네트워크 오류 처리
-                    print('네트워크 오류: $error');
-                  });
+                  searchResult = searchAddress(controller.text);
+                  (context as Element).markNeedsBuild();
                 },
                 child: const Text('검색'),
+              ),
+              Expanded(
+                child: FutureBuilder<List<Map<String, dynamic>>>(
+                  future: searchResult,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      return Text('오류: ${snapshot.error}');
+                    } else if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data?.length ?? 0,
+                        itemBuilder: (context, index) {
+                          var address = snapshot.data![index];
+                          return ListTile(
+                            title: Text(
+                                '${address["originalLandAddress"]} / ${address["originalRoadAddress"]}'),
+                            subtitle: Text(
+                                '위도: ${address["point"]["latitude"]}, 경도: ${address["point"]["longitude"]}'),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Text('검색 결과가 없습니다.');
+                    }
+                  },
+                ),
               ),
             ],
           ),
